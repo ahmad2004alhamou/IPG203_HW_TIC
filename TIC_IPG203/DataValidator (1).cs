5823380138db38c66a3294ef1332d52bed752160
sedraghanem083-sudo <sedraghanem083@gmail.com>
2026-05-14 09:26:07 +0300

    Add files via upload

---
+++ TIC_IPG203/DataValidator (1).cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPG203_OOP
+{
+    public static class DataValidator
+    {
+        public static bool IsValidIndex(int index, int length)// للتحقق من ان هل الرقم اللي أدخله المستخدم يقع ضمن حدود المصفوفة 
+        {
+            return index >= 0 && index < length;
+        }
+
+       
+
+        public static bool IsPositive(decimal value)//للتحقق ما اذا كان الرقم موجب ام لا
+        {
+            return value > 0;
+        }
+    }
+
+}
---
+++ TIC_IPG203/OrderTracker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPG203_OOP
+{
+    public class OrderTracker
+    {
+        public static int TotalOrders { get; private set; }
+
+        public static void AddOrder()//مثود لحساب عدد الطلبات الاجمالي في كل الاقسام
+        {
+            TotalOrders++;
+        }
+    }
+
+}
